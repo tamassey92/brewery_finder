9a06b442d579242688e3ee5711fd7544f1a45a4c
Taylor Massey <tamassey92@gmail.com>
2023-02-09 22:03:50 -0500

    added db.json data

---
+++ index.js
@@ -0,0 +1,29 @@
+
+fetch("http://localhost:3000/breweries")
+.then((res) => res.json())
+.then(data => displayBreweries(data))
+
+function displayBreweries(breweries) {
+    const container = document.getElementById('beer-container')
+    breweries.forEach((brewery) =>{
+        const breweryList = breweryCard(brewery)
+        container.append(breweryList)
+    })
+}
+
+function breweryCard(brewery) {
+    const div = document.createElement('div')
+    const name = document.createElement('h2')
+    // const website = document.createElement('a')
+    const image = document.createElement('img')
+    const location = document.createElement('h4')
+
+    name.textContent = brewery.name
+    // website.setAttribute('href', '#')
+    // website.textContent = brewery.website_url
+    location.textContent = brewery.city brewery.state
+    image.src = brewery.image
+
+    div.append(name, location, image)
+    return div
+}
