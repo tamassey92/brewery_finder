a3c86b554c50fa11d0fc3d6d0c320740b319a23e
Taylor Massey <tamassey92@gmail.com>
2023-03-15 13:50:32 -0400

    style alignment

--- index.js
+++ index.js
@@ -13,17 +13,19 @@ function displayBreweries(breweries) {
 
 function breweryCard(brewery) {
     const div = document.createElement('div')
+    div.className = "beer-card"
     const name = document.createElement('h2')
     // const website = document.createElement('a')
     const image = document.createElement('img')
+    image.className = "brewery-image"
     const location = document.createElement('h4')
 
     name.textContent = brewery.name
     // website.setAttribute('href', '#')
     // website.textContent = brewery.website_url
-    location.textContent = brewery.city brewery.state
+    location.textContent = `${brewery.city}, ${brewery.state}`
     image.src = brewery.image
 
-    div.append(name, location, image)
+    div.append(name, image, location)
     return div
 }
